174b9f9c76b32252dddc596a231879e185af7490
ponpitak <bankppt.srm@gmail.com>
2024-02-24 22:05:02 +0700

    update not found page

--- src/views/NotFoundPage.tsx
+++ src/views/NotFoundPage.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function NotFoundPage() {
   return (
-    <div className="container-main pt-52 h-fit">
-      <div className="flex flex-col items-center justify-center">
+    <div className="container-main pt-32 h-fit">
+      <div className="flex flex-col items-center justify-center py-20 px-32 bg-white rounded-[10px]">
         <h1 className="font-bold">Oops...</h1>
         <h5 className="text-violet py-5">404 - Not Found Page</h5>
         <div className="container-btn">
-          <Link to="/" className="btn-base">
+          <Link to="/" className="btn-base btn-sub">
             Return Homepage
           </Link>
         </div>
